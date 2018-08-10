2477f0f169cf1a42d4ff1cc411d358dfb834eb4a
John <John@gmail.com>
2018-08-10 11:20:02 +0530

    Changing version

--- src/main/java/com/lms/controller/AuthenticationController.java
+++ src/main/java/com/lms/controller/AuthenticationController.java
@@ -23,6 +23,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 @RestController
 @RequestMapping("/api1")
 public class AuthenticationController {
+	
 	@Autowired
 	private IEmployeeService employeeService;
 	@Autowired
@@ -30,11 +31,13 @@ public class AuthenticationController {
 	@Autowired
 	private RedisTemplate<String, Object> template;
 	
+	String ename;
 	@PostMapping("/loginUserName")
 	public  Map<String,String> loginUsername(@RequestBody Employee employee) {
 		Map<String,String> map=new HashMap<>();
 		Employee employee1 = employeeService.findByUserName(employee.getUserName());
-
+		ename=employee1.getUserName();
+		System.out.println(employee1);
 		if (employee1 == null) {
 			map.put("message","Employee does not exist") ;
 			map.put("status",HttpStatus.CONFLICT.toString());
@@ -49,33 +52,43 @@ public class AuthenticationController {
 		return map;
 		
 	}
-	@PostMapping("/loginPassword")
-	public String loginPassword(@RequestBody Employee employee) {
-	
-		{
-			Employee employee1 = employeeService.findByUserName(employee.getUserName());
-			
-			if (employee1.getPassword().equals(employee.getPassword())) {
-					try {
-						Object token = Jwts.builder().setSubject("users/TzMUocMF4p").setExpiration(new Date(1300819380))
-								.claim("name", "Robert Token Man").claim("scope", "self groups/admins")
-								.signWith(SignatureAlgorithm.HS256, "secret".getBytes("UTF-8")).compact();
-						redis.setValue(employee.getUserName(), token);
-						
-						 //userService.registerUser(user); 
-						return "Successfully Login";
-					} catch (UnsupportedEncodingException e) {
 
-						e.printStackTrace();
-					}
-				} else {
-					
-					return "Wrong Password";
-					//userService.registerUser(user1);
+	@PostMapping("/loginPassword")
+	public Map<String,String> loginPassword(@RequestBody Employee employee) {
+		Map<String,String> map=new HashMap<>();
+		employee.setUserName(ename);
+		Employee employee1 = employeeService.findByUserName(employee.getUserName());
+		System.out.println(employee1);
+		if (employee1 != null) {
+			/*
+			 * if(employee.getPassword()==null) { return
+			 * "Please enter Password"; }
+			 */
+			if (employee1.getPassword().equals(employee.getPassword())) {
+				try {
+					Object token = Jwts.builder().setSubject("users/TzMUocMF4p").setExpiration(new Date(1300819380))
+							.claim("name", "Robert Token Man").claim("scope", "self groups/admins")
+							.signWith(SignatureAlgorithm.HS256, "secret".getBytes("UTF-8")).compact();
+					redis.setValue(employee.getUserName(), token);
+
+					// userService.registerUser(user);
+					map.put("message","Valid Password") ;
+					map.put("status",HttpStatus.ACCEPTED.toString());
+
+				} catch (UnsupportedEncodingException e) {
+
+					e.printStackTrace();
 				}
-			
+			} else {
+
+				map.put("message","Wrong Password") ;
+				map.put("status",HttpStatus.CONFLICT.toString());
+
+				// userService.registerUser(user1);
+			}
+
 		}
-		return "";
+		return map;
 
 	}
 	
