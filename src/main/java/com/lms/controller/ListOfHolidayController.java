2e3ac0daae15da2605c79b86c06bdf4c001e1f54
John <John@gmail.com>
2018-08-14 14:46:10 +0530

    Testing api

--- src/main/java/com/lms/controller/ListOfHolidayController.java
+++ src/main/java/com/lms/controller/ListOfHolidayController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -40,6 +41,11 @@ public class ListOfHolidayController {
 		iListOfHolidaysService.registerHoliday(listOfHolidays);
 
 				
+	}
+	@PostMapping("/test")
+	public String test()
+	{
+		return "test";
 	}
 
 }
