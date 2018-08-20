aec36acd35f9577aed23417407960cb26c3205c5
John <John@gmail.com>
2018-08-20 14:23:50 +0530

    Successful Test Packages

--- src/test/java/com/lms/LeaveManagementSystemApplicationTests.java
+++ src/test/java/com/lms/LeaveManagementSystemApplicationTests.java
@@ -371,5 +371,6 @@ public class LeaveManagementSystemApplicationTests {
 	e.printStackTrace();
 	}
 	}
+	
 }
  
---
+++ src/test/java/com/lms/addUsers/AddUserTestCases.java
@@ -0,0 +1,24 @@
+package com.lms.addUsers;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.lms.LeaveManagementSystemApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LeaveManagementSystemApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(LeaveManagementSystemApplication.class)
+public class AddUserTestCases {
+	@Test
+	public void testEquals1()
+	{
+		String str="Hello";
+		assertEquals("Hello",str);
+	}
+
+}
